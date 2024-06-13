e2f0fa29bd2fd67060ee730ddc6f88f206a11c1e
kevin <kevin.murray1116@gmail.com>
2024-06-13 13:31:11 -0700

    header "I HOPE" works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import ThreeScene from './ThreeScene';
-import PlanetInfo from './PlanetInfo'; // This will be our UI component to show on click
+import React, { useEffect, useState } from "react";
+import ThreeScene from "./ThreeScene";
+import PlanetInfo from "./PlanetInfo"; // This will be our UI component to show on click
+import Header from "./Header.js"; // This will be <self-explanatory>
 
 const App = () => {
   const [showPlanetInfo, setShowPlanetInfo] = useState(false);
   const [selectedPlanetId, setSelectedPlanetId] = useState(null);
   const [planetDatas, setPlanetDatas] = useState([]);
-  const [loading, setLoading]  = useState(true);
+  const [loading, setLoading] = useState(true);
 
   const handlePlanetClick = (input) => {
     setSelectedPlanetId(input);
@@ -15,12 +16,12 @@ const App = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch("https://swapi.dev/api/planets");
-        const results = await response.json();
-        const data = results.results;
-        setPlanetDatas(data);
-        setLoading(false);
-    }
+      const response = await fetch("https://swapi.dev/api/planets");
+      const results = await response.json();
+      const data = results.results;
+      setPlanetDatas(data);
+      setLoading(false);
+    };
     fetchData();
   });
 
@@ -28,10 +29,23 @@ const App = () => {
   //Test
   return (
     <>
-    <div>
-      {!loading && <ThreeScene onPlanetClick={handlePlanetClick} planetDatas={planetDatas} planetNames={["test1", "test2", "test3", "test4", "test5"]}/>}
-      {showPlanetInfo && <PlanetInfo planetId={selectedPlanetId} setShowPlanetInfo={setShowPlanetInfo} showPlanetInfo={showPlanetInfo} />}
-    </div>
+      <div>
+        {!loading && <Header />}
+        {!loading && (
+          <ThreeScene
+            onPlanetClick={handlePlanetClick}
+            planetDatas={planetDatas}
+            planetNames={["test1", "test2", "test3", "test4", "test5"]}
+          />
+        )}
+        {showPlanetInfo && (
+          <PlanetInfo
+            planetId={selectedPlanetId}
+            setShowPlanetInfo={setShowPlanetInfo}
+            showPlanetInfo={showPlanetInfo}
+          />
+        )}
+      </div>
     </>
   );
 };
