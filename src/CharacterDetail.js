b330ec959032c7b13257f3cec0b28b52b068f394
TIFFANY <crick.tiffany@yahoo.com>
2024-06-17 16:07:42 -0400

    modified Character Details

--- src/CharacterDetail.js
+++ src/CharacterDetail.js
@@ -1,26 +1,36 @@
 
 
-const CharacterDetail = ({character, setSelectedCharacter}) => {
+const CharacterDetail = ({ character, setSelectedCharacter }) => {
 
     return (
-        <div className = "character-detail" 
-        onClick={()=>{setSelectedCharacter(false)}}
-        style={{
-            position: "absolute",
-            top: 120,
-            left: "2%",
-            padding: "20px",
-            backgroundColor: "#333",
-            opacity: 0.9,
-            borderRadius: "8px",
-            zIndex: 1000,
+        <div className="character-detail"
+            onClick={() => { setSelectedCharacter(false) }}
+            style={{
+                position: "absolute",
+                top: 120,
+                left: "2%",
+                padding: "20px",
+                backgroundColor: "#333",
+                opacity: 0.9,
+                borderRadius: "8px",
+                zIndex: 1000,
 
-            maxHeight: "80vh", // Maximum height of the dialog box
-            overflowY: "auto", // Enable vertical scrolling if content exceeds max height
-        }}>
-        
+                maxHeight: "80vh", // Maximum height of the dialog box
+                overflowY: "auto", // Enable vertical scrolling if content exceeds max height
+            }}>
+
             <h1>{character.name}</h1>
             <img src={character.image} width="150"></img>
+            <p>Homeworld: {`${character.homeworld[0].toUpperCase()}${character.homeworld.slice(1)}`}</p>
+            <p>Species: {character.species}</p>
+            <p>Affiliations:
+                {character.affiliations.map(elem => (
+                    <ul
+                        style={{ fontSize: "10pt" }}>
+                        {elem}
+                    </ul>
+                ))}
+            </p>
 
         </div>
     )
--- src/CharacterList.js
+++ src/CharacterList.js
@@ -1,10 +1,13 @@
 import { useState } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import characterData from './characters';
 import Character from './Character';
 import CharacterDetail from './CharacterDetail';
 
 const CharacterList = () => {
     const [selectedCharacter, setSelectedCharacter] = useState(false);
+    const navigate = useNavigate();
 
     if (selectedCharacter) {
         return (
@@ -12,6 +15,15 @@ const CharacterList = () => {
                 setSelectedCharacter={setSelectedCharacter} />
         )
     }
+
+    const handleCloseClick = (e) => {
+        e.stopPropagation(); // Prevent the click event from propagating to the parent element
+        // if (planetName) {
+            navigate("/");
+        // }
+        // setShowPlanetInfo(false);
+    };
+
     return (
         <div className="character-list"
             style={{
@@ -27,6 +39,21 @@ const CharacterList = () => {
                 maxHeight: "80vh", // Maximum height of the dialog box
                 overflowY: "auto", // Enable vertical scrolling if content exceeds max height
             }}>
+            <button
+                onClick={handleCloseClick}
+                style={{
+                    position: "absolute",
+                    top: "5px",
+                    right: "5px",
+                    backgroundColor: "transparent",
+                    border: "none",
+                    color: "white",
+                    fontSize: "20px",
+                    cursor: "pointer",
+                }}
+            >
+                ×
+            </button>
             {characterData.map(character => (
                 <Character character={character}
                     setSelectedCharacter={setSelectedCharacter} />))}
