cfa877eaf3355607ed826b78b511c9761514a2f2
kevin <kevin.murray1116@gmail.com>
2024-06-17 09:07:30 -0700

    cmmoiititi

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import appLogo from "./app-logo.png";
 //import {handleButtonClick} from "./ThreeScene.js";
 
 function Header({
   planetDatas,
-  setMenuPlanetSelected
+  setMenuPlanetSelected,
   /*Array of Planet Objects, names to be used to populate option list */
 }) {
-  
-
-  //NAvigate onclick to planet
-  // set camera planets[index].position
-  const handleChange = (event) => {
-      const selectedPlanetName = event.target.value;
-      // Find the corresponding planet object based on the selected name
-      const selectedPlanet = planetDatas.find(planet => planet.name === selectedPlanetName);
-  
-      // If a planet is found, trigger handleButtonClick with the planet ID
-      if (selectedPlanet) {
-        //find selected planet data in planetDatas
-        for(let i = 0; i< planetDatas.length; i++)
-          {
-            if(planetDatas[i].name === selectedPlanet)
-              {
-                setMenuPlanetSelected(i);
-              }
-          }
-
-        //setMenuPlanetSelected(selectedPlanet);
-        //handleButtonClick(selectedPlanet.id); // Assuming planet object has an 'id' property
-      }
-    };
- 
   return (
     <div className="header">
       <div>
@@ -40,13 +15,12 @@ function Header({
       </div>
       <div className="nav-planets">
         <label>Choose a planet:</label>
-        <select className="select" id="planets" name="planets" onChange={handleChange}>
+        <select className="select" id="planets" name="planets">
           {planetDatas.map((index) => (
-              <option className="options" value={index.name} >
-                {index.name}
-              </option>
-            ))
-          }
+            <option className="options" value={index}>
+              {index.name}
+            </option>
+          ))}
         </select>
       </div>
       <div className="nav-movies-characters">
